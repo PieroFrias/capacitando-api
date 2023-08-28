ad7abd1db8fcd0e6f91f0aafe2f968f58bdaf3b6
PieroFrias <piefrimo@gmail.com>
2023-08-27 22:50:36 -0500

    Merge development (categories)

--- src/interfaces/router/index.routes.js
+++ src/interfaces/router/index.routes.js
@@ -2,15 +2,15 @@ import express from "express";
 import coursesRouter from "./courses.routes.js";
 import usersRouter from "./users.routes.js";
 import sessionsRouter from "./sessions.routes.js";
-import filesRouter from "./files.routes.js"
 import categoriesRouter from "./categories.routes.js"
+import filesRouter from "./files.routes.js"
 
 const router = express.Router();
 
 router.use("/courses", coursesRouter);
 router.use("/users", usersRouter);
 router.use("/sessions", sessionsRouter);
-router.use("/data/images", filesRouter);
 router.use("/categories", categoriesRouter);
+router.use("/data/images", filesRouter);
 
 export default router;
