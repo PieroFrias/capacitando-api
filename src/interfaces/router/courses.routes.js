e24ef00507fa3722e90150d0f39e6aa62b6d9d85
PieroFrias <piefrimo@gmail.com>
2023-08-30 11:03:53 -0500

    Add courseValidator middleware

--- src/interfaces/router/courses.routes.js
+++ src/interfaces/router/courses.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import checkAuth from '../middleware/authMiddleware.js';
 import checkRole from '../middleware/roleMiddleware.js';
 import { validateStatus } from "../middleware/validators/statusValidator.js";
+import { validateCreate, validateEdit } from "../middleware/validators/courseValidator.js";
 import {
   getAllCourses,
   getAllCoursesPaginated,
@@ -22,10 +23,10 @@ router.get("/detail/:id", checkAuth, getCourseDetail);
 router.get("/list", checkAuth, getAllCourses);
 router.get("/list-paginate", checkAuth, getAllCoursesPaginated);
 
-router.post("/create", checkAuth, checkRole([1]), createCourse);
+router.post("/create", checkAuth, checkRole([1]), validateCreate, createCourse);
 router.post("/add/user", checkAuth, checkRole([1]), addCourseUser);
 
-router.patch("/update/:id", checkAuth, checkRole([1]), updateCourse);
+router.patch("/update/:id", checkAuth, checkRole([1]), validateEdit, updateCourse);
 
 router.put("/image/:id", checkAuth, checkRole([1]), uploadImage.single("url_portada"), addUpdateImageCourse);
 router.put("/status/:id", checkAuth, checkRole([1]), validateStatus, changeStatusCourse);
